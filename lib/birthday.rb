824f006b1b6eaae242c57d0dc80dba564cabd9a8
Bruce Parker <detailsharks@gmail.com>
2020-06-09 23:36:13 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do  |name, age|
     puts "Happy Birthday Timmy!Happy Birthday Sarah! You are now 6 years old! You are too old for this"
+    
   end
 end
 
