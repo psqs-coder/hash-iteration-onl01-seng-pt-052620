c7b53881c1b98e5d39b56c96b621bc4fa69202be
Bruce Parker <detailsharks@gmail.com>
2020-06-09 23:07:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,11 +5,15 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
-  birthday_kids.each do |kids_name,age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do  |name, age|
+    puts "Happy Birthday Timmy! Happy Birthday Sarah! You are now 6 years old! You are too old for this"
+  end
+end
 
 
